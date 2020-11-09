aeda9a52001fd9eff323676a8c604415af2e94c3
Valentina Ponamareva <valiazin@gmail.com>
2020-11-09 18:02:42 +0300

    feat: add checking unsolvable sequences

--- gem-puzzle/src/main.js
+++ gem-puzzle/src/main.js
@@ -13,15 +13,16 @@ main.appendChild(field);
 document.body.appendChild(main);
 
 //
-const empty = {
-  value: 0,
-  top: 0,
-  left: 0
-}
 const cellSize = 100;
+const fieldSize = 4;
+
+const empty = {
+  value: fieldSize * fieldSize,
+  top: fieldSize - 1,
+  left: fieldSize - 1
+}
 
 const cells = [];
-cells.push(empty);
 
 const move = (index) => {
   const cell = cells[index];
@@ -45,7 +46,7 @@ const move = (index) => {
 
   //check if we win
   const isFinished = cells.every(cell => {
-    return cell.value === cell.top * 4 + cell.left;
+    return cell.value === cell.top * 4 + cell.left + 1;
   });
 
   if (isFinished) {
@@ -54,17 +55,52 @@ const move = (index) => {
 }
 
 //random array
-const numbers = [...Array(15).keys()].sort(() => Math.random() - 0.5);
+const numbers = [...Array(15).keys()].map(x => x + 1);
+let randomArr = [];
 
-for (let i = 1; i <= 15; i++) {
+const generateRandomNumbers = (arr) => {
+  return randomArr = arr.sort(() => Math.random() - 0.5);
+}
+const randomNumbers = numbers.sort(() => Math.random() - 0.5);
+
+// check if the random sequence has solution
+const checkSequence = (arr) => {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let counter = 0;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        counter++;
+      }
+    }
+    sum += counter;
+  }
+  sum += 4;
+  if (sum % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let isSequenceSolvable = false;
+
+while (!isSequenceSolvable) {
+  generateRandomNumbers(numbers);
+  console.log(randomArr, checkSequence(randomArr));
+  isSequenceSolvable = checkSequence(randomArr);
+}
+
+for (let i = 0; i <= 14; i++) {
   const cell = document.createElement('div');
-  const value = numbers[i - 1] + 1;
+  const value = randomNumbers[i];
   cell.classList.add("cell");
   cell.textContent = value;
 
-  const cellsInRow = 4;
-  const left = i % cellsInRow;
-  const top = (i - left) / 4;
+  const left = i % fieldSize;
+  const top = (i - left) / fieldSize;
 
   cells.push({
     value: value,
@@ -82,3 +118,5 @@ for (let i = 1; i <= 15; i++) {
     move(i);
   })
 }
+
+cells.push(empty);
