e1ff0c9d3832726119c8d726322f0c93910d1c87
Valentina Ponamareva <valiazin@gmail.com>
2020-11-13 19:02:17 +0300

    feat: implement drag n drop

--- gem-puzzle/src/scripts/cell.js
+++ gem-puzzle/src/scripts/cell.js
@@ -18,12 +18,67 @@ export default class Cell {
     cellDom.style.left = `${this.left * this.cellSize}px`;
     cellDom.style.top = `${this.top * this.cellSize}px`;
 
+
+    cellDom.onmousedown = (event) => {
+      const cellLeft = cellDom.style.left;
+      const cellTop = cellDom.style.top;
+
+      const shiftX = event.clientX - cellDom.getBoundingClientRect().left;
+      const shiftY = event.clientY - cellDom.getBoundingClientRect().top;
+
+      cellDom.style.transitionProperty = 'none';
+      cellDom.style.zIndex = 100;
+      document.body.append(cellDom);
+      
+      function moveAt(pageX, pageY) {
+        cellDom.style.left = pageX - shiftX + 'px';
+        cellDom.style.top = pageY - shiftY + 'px';
+      }
+
+      moveAt(event.pageX, event.pageY);
+      
+      function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+      }
+
+      document.addEventListener('mousemove', onMouseMove);
+
+      cellDom.onmouseup = (event) => {
+        cellDom.style.display = 'none';
+        const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        cellDom.style.display = 'flex';
+
+        if (!elemBelow) return;
+
+        if (elemBelow.className === 'field') {
+          cellDom.style.left = cellLeft;
+          cellDom.style.top = cellTop;
+          cellDom.style.zIndex = 'auto';
+          document.querySelector('.field').append(cellDom);
+
+          this._onclick();
+        } else {
+          cellDom.style.left = cellLeft;
+          cellDom.style.top = cellTop;
+          cellDom.style.zIndex = 'auto';
+          document.querySelector('.field').append(cellDom);
+        }
+
+        document.removeEventListener('mousemove', onMouseMove);
+        cellDom.onmouseup = null;
+        cellDom.style.transitionProperty = 'all';
+      };
+    }
+    cellDom.ondragstart = function() {
+      return false;
+    };
     cellDom.addEventListener('click', () => this._onclick());
 
     return cellDom;
   }
 
   _onclick() {
+    console.log('click');
     const event = new CustomEvent('clickCell', {
       bubbles: true, 
       detail: this 
